262113d8be93980656c578b564d3b6e96662ae9a
Mai <mai.than.222@gmail.com>
2024-11-29 09:54:22 +0100

    Test 1 page Admin

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def admin_dashboard(request):
+    context = {
+        "admin" : "Administrator"
+        }
+    return render(request, 'admin_dashboard.html', context=context)
