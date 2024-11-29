7ed17dfb814dea994b52e33f7023adf7c1e1e879
Mai <mai.than.222@gmail.com>
2024-11-29 10:12:41 +0100

    migrate Demande Conge model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class DemandeConge(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    type_congee = models.CharField(max_length=100)
+    date_debut = models.DateField()
+    date_fin = models.DateField()
+    motif = models.TextField()
+    date_demande = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "Demande faite par " + self.nom + " " + self.prenom
+        
