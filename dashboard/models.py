b247bf463e0a0aba805330603402bf2a8e3903fa
Mai <mai.than.222@gmail.com>
2024-11-29 10:14:09 +0100

    added Employe model + migrate

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,3 +13,15 @@ class DemandeConge(models.Model):
     def __str__(self):
         return "Demande faite par " + self.nom + " " + self.prenom
         
+class Employe(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField()
+    date_embauche = models.DateField()
+    date_naissance = models.DateField()
+    adresse = models.TextField()
+    telephone = models.CharField(max_length=15)
+    poste = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nom + " " + self.prenom
